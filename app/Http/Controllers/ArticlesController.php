823eb5cc92ab0b9d674cc366b6b9c7352fb1a141
Zepheo <douglas.eskelius@gmail.com>
2020-08-24 09:35:30 +0200

    ep22

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -13,10 +13,12 @@ class ArticlesController extends Controller
     public function index()
     {
       $articles = Article::latest()->get();
+
       return view('articles.index', ['articles' => $articles]);
     }
     public function show($id) {    
-        $article = Article::find($id);
-        return view('articles.show', ['article' => $article]);
-      }
+      $article = Article::find($id);
+
+      return view('articles.show', ['article' => $article]);
+    }
 }
